ad3d39230e650001ba8c947a7915eb41919c68c5
irvinec <caseyi@outlook.com>
2019-02-02 22:26:21 -0800

    WIP: Trying to integrate with win APIs
    I have the very early start of trying to integrate with windows device APIs.
    
    Right now it is not compiling so this is really just saving wip.

--- winble/winble.cpp
+++ winble/winble.cpp
@@ -1,6 +1,6 @@
 #include <pybind11/pybind11.h>
-//#include <winrt/Windows.Devices.Bluetooth.h>
-#include <winrt/base.h>
+#include <winrt/Windows.Devices.Bluetooth.h>
+#include <winrt/Windows.Devices.Enumeration.h>
 
 // Link to umbrella lib.
 #pragma comment(lib, "windowsapp")
@@ -27,6 +27,28 @@ struct WinBleAdapter
 
     py::list Scan()
     {
+        // Query for extra properties you want returned
+        std::vector<std::string> requestedProperties = { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected" };
+
+        Windows::Devices::Enumeration::DeviceWatcher deviceWatcher =
+            Winodws::Devices::Enumeration::DeviceInformation::CreateWatcher(
+                    Winodws::Devices::Bluetooth::BluetoothLEDevice::GetDeviceSelectorFromPairingState(false),
+                    requestedProperties,
+                    Winodws::Devices::Enumeration::DeviceInformationKind::AssociationEndpoint);
+
+        // Register event handlers before starting the watcher.
+        // Added, Updated and Removed are required to get all nearby devices
+        //deviceWatcher.Added += DeviceWatcher_Added;
+        //deviceWatcher.Updated += DeviceWatcher_Updated;
+        //deviceWatcher.Removed += DeviceWatcher_Removed;
+
+        // EnumerationCompleted and Stopped are optional to implement.
+        //deviceWatcher.EnumerationCompleted += DeviceWatcher_EnumerationCompleted;
+        //deviceWatcher.Stopped += DeviceWatcher_Stopped;
+
+        // Start the watcher.
+        deviceWatcher.Start();
+
         py::list devices;
         py::dict bogusDevice;
         bogusDevice["name"] = "SK-Bogus";
@@ -34,6 +56,9 @@ struct WinBleAdapter
         devices.append(bogusDevice);
         return devices;
     }
+
+private:
+
 };
 
 PYBIND11_MODULE(winble, m)
